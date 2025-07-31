1c9f3c8d961a545d4add87104593c3f3710751c0
pedromfbesteves-png <pedro.mfb.esteves@gmail.com>
2025-07-31 19:44:56 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Galeria</title>
+</head>
+<body>
+    <h1>Galeria de Imagens</h1>
+    <p><a href="upload.php">Ir para upload de imagens</a></p>
+</body>
+</html>
